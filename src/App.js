52fed6c8b6a58b5c3faed2735b8aebb69f2ce722
berger <robertberger.dev@gmail.com>
2023-02-24 15:50:07 -0500

    added css elements to the list items

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ function App() {
   let name = 'Robert';
   let dogs = ['Fido', 'Buffy', 'Rex'];
 
-  let dogListItems = dogs.map(dog => (<li>{dog}</li>));
+  let dogListItems = dogs.map(dog => {
+    return (<li className="dog-name">{dog}</li>);
+  })
 
   return (
     <div className="App">
       <h1>Hello {name}!</h1>
-      <ul>
-        { dogListItems }
-      </ul>
+      {
+        dogs.length == 0
+          ? <p>No Dogs</p>
+          :(<ul>
+            { dogListItems }
+            </ul>)
+      }
     </div>
   );
 }
