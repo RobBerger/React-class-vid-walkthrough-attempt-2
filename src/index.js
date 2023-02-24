2f8c1023255b36b4b40f8aff3c055891ace530ef
berger <robertberger.dev@gmail.com>
2023-02-23 19:02:26 -0500

    got the example to work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const heading = React.createElement('h1', null, 'Hello World!');
+
+const l1 = React.createElement('li', null, 'Bananas');
+const l2 = React.createElement('li', null, 'Milk');
+const l3 = React.createElement('li', null, 'Bread');
+
+const groceryList = React.createElement('ol', null, [l1, l2, l3]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    [heading, groceryList]
+)
+
+
+
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
