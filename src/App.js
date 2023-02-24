5236a3c4ce5fba50370f759d697ba4cf80ffbeb2
berger <robertberger.dev@gmail.com>
2023-02-23 19:56:56 -0500

    moved map function to variable and called the let in its place

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
   let name = 'Robert';
   let dogs = ['Fido', 'Buffy', 'Rex'];
 
+  let dogListItems = dogs.map(dog => (<li>{dog}</li>));
+
   return (
     <div className="App">
       <h1>Hello {name}!</h1>
       <ul>
-        {
-          dogs.map(dog => (<li>{dog}</li>))
-        }
+        { dogListItems }
       </ul>
     </div>
   );
